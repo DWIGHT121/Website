9cfee180528d7b1f42cbdd9a16fb343e775b7bae
DWIGHT121 <66079119+DWIGHT121@users.noreply.github.com>
2020-08-21 19:48:05 +0545

    ss

--- writing/articles/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from .models import Visitors
-
-
-class RegisterForm(ModelForm):
-    class Meta:
-        model = Visitors
-        fields = '__all__'
